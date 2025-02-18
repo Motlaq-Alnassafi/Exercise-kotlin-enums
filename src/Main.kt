f1f0afe50b84151902b932c80faf8b4700d1f31f
Motlaq-Alnassafi <alnassafimotlaq@gmail.com>
2025-02-18 19:09:31 +0300

    Motlaq Alnesafi

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,14 @@
 fun main() {
-    println("Hello World!")
+    var user = UserRole.VIEWER
+    checkAccessLevel(user)
+    for (user in UserRole.values()) {
+        println("userName: ${user.name}, accessLevel: ${user.accessLevel}")
+    }
+}
+enum class UserRole(val accessLevel: Int) {
+    ADMIN(3), EDITOR(2), VIEWER(1)
+}
+
+fun checkAccessLevel(userRole: UserRole) {
+    if (userRole.accessLevel == 3) println("User has the required access level") else println("User does not have the required access level")
 }
